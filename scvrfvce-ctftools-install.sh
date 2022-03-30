650224ca7fdb5a2df5178d69bb62887be3e466a8
x1mus <laenen.maximilien@outlook.com>
2022-03-30 10:15:31 -0400

    [upd] apt-key to gpg & voladpro fixed

--- scvrfvce-ctftools-install.sh
+++ scvrfvce-ctftools-install.sh
@@ -44,8 +44,8 @@ echo "OK"
 
 # Sublime Text 3
 echo -n "Installing Sublime Text 3......................."
-wget -O /tmp/sublime-text.gpg https://download.sublimetext.com/sublimehq-pub.gpg -q
-sudo apt-key add /tmp/sublime-text.gpg &> /dev/null
+wget -O - https://download.sublimetext.com/sublimehq-pub.gpg -q | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/sublime-text.gpg --import &> /dev/null
+sudo chmod 644 /etc/apt/trusted.gpg.d/sublime-text.gpg
 sudo apt-get install -y apt-transport-https > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null
 sudo apt-get update > /dev/null
